a9598b249cc5923f52b507c072c700a254f3d5a5
naga-karthik <53445351+naga-karthik@users.noreply.github.com>
2025-09-03 14:47:30 +0000

    Deploying to gh-pages from @ naga-karthik/naga-karthik.github.io@205460bd7328685931d220bfa95cf94e8a7f2cd6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,345 +30,59 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-keeping-up-with-research",
         
-          title: "a post with plotly.js",
+          title: "Keeping up with research",
         
-        description: "this is what included plotly.js code could look like",
+        description: "Insights on reading and managing the literature",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/plotly/";
+            window.location.href = "/blog/2023/managing-research/";
           
         },
-      },{id: "post-a-post-with-image-galleries",
+      },{id: "post-getting-started-with-machine-learning-and-neuroimaging",
         
-          title: "a post with image galleries",
+          title: "Getting started with machine learning and neuroimaging",
         
-        description: "this is what included image galleries could look like",
+        description: "Some useful resources for getting started with machine learning and neuroimaging.",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/photo-gallery/";
+            window.location.href = "/blog/2023/resources-ml-and-neuroimaging/";
           
         },
-      },{id: "post-a-post-with-tabs",
+      },{id: "post-the-book-that-resonated-with-me-the-most",
         
-          title: "a post with tabs",
+          title: "The book that resonated with me the most",
         
-        description: "this is what included tabs in a post could look like",
+        description: "A long review of a memoir based on running.",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/tabs/";
+            window.location.href = "/blog/2023/murakami-running-book/";
           
         },
-      },{id: "post-a-post-with-typograms",
+      },{id: "post-list-of-books-i-read-in-2022",
         
-          title: "a post with typograms",
+          title: "List of Books I Read in 2022",
         
-        description: "this is what included typograms code could look like",
+        description: "A list of books I read in 2022 and my (brief) thoughts on them.",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/typograms/";
+            window.location.href = "/blog/2023/books-in-2022/";
           
         },
-      },{id: "post-a-post-that-can-be-cited",
+      },{id: "post-list-of-books-i-read-in-2020",
         
-          title: "a post that can be cited",
+          title: "List of Books I Read in 2020",
         
-        description: "this is what a post that can be cited looks like",
+        description: "A list of books I read in 2020 and my brief opinion on them",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/post-citation/";
-          
-        },
-      },{id: "post-a-post-with-pseudo-code",
-        
-          title: "a post with pseudo code",
-        
-        description: "this is what included pseudo code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/pseudocode/";
-          
-        },
-      },{id: "post-a-post-with-code-diff",
-        
-          title: "a post with code diff",
-        
-        description: "this is how you can display code diffs",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/code-diff/";
-          
-        },
-      },{id: "post-a-post-with-advanced-image-components",
-        
-          title: "a post with advanced image components",
-        
-        description: "this is what advanced image components could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/advanced-images/";
-          
-        },
-      },{id: "post-a-post-with-vega-lite",
-        
-          title: "a post with vega lite",
-        
-        description: "this is what included vega lite code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/vega-lite/";
-          
-        },
-      },{id: "post-a-post-with-geojson",
-        
-          title: "a post with geojson",
-        
-        description: "this is what included geojson code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/geojson-map/";
-          
-        },
-      },{id: "post-a-post-with-echarts",
-        
-          title: "a post with echarts",
-        
-        description: "this is what included echarts code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/echarts/";
-          
-        },
-      },{id: "post-a-post-with-chart-js",
-        
-          title: "a post with chart.js",
-        
-        description: "this is what included chart.js code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/chartjs/";
-          
-        },
-      },{id: "post-a-post-with-tikzjax",
-        
-          title: "a post with TikZJax",
-        
-        description: "this is what included TikZ code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tikzjax/";
-          
-        },
-      },{id: "post-a-post-with-bibliography",
-        
-          title: "a post with bibliography",
-        
-        description: "an example of a blog post with bibliography",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/post-bibliography/";
-          
-        },
-      },{id: "post-a-post-with-jupyter-notebook",
-        
-          title: "a post with jupyter notebook",
-        
-        description: "an example of a blog post with jupyter notebook",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/jupyter-notebook/";
-          
-        },
-      },{id: "post-a-post-with-custom-blockquotes",
-        
-          title: "a post with custom blockquotes",
-        
-        description: "an example of a blog post with custom blockquotes",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/custom-blockquotes/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
-        
-          title: "a post with table of contents on a sidebar",
-        
-        description: "an example of a blog post with table of contents on a sidebar",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/sidebar-table-of-contents/";
-          
-        },
-      },{id: "post-a-post-with-audios",
-        
-          title: "a post with audios",
-        
-        description: "this is what included audios could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/audios/";
-          
-        },
-      },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/videos/";
-          
-        },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tables/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents",
-        
-          title: "a post with table of contents",
-        
-        description: "an example of a blog post with table of contents",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/table-of-contents/";
-          
-        },
-      },{id: "post-a-post-with-giscus-comments",
-        
-          title: "a post with giscus comments",
-        
-        description: "an example of a blog post with giscus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2022/giscus-comments/";
-          
-        },
-      },{id: "post-a-post-with-redirect",
-        
-          title: "a post with redirect",
-        
-        description: "you can also redirect to assets like pdf",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/assets/pdf/example_pdf.pdf";
-          
-        },
-      },{id: "post-a-post-with-diagrams",
-        
-          title: "a post with diagrams",
-        
-        description: "an example of a blog post with diagrams",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/diagrams/";
-          
-        },
-      },{id: "post-a-distill-style-blog-post",
-        
-          title: "a distill-style blog post",
-        
-        description: "an example of a distill-style blog post and main elements",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/distill/";
-          
-        },
-      },{id: "post-a-post-with-twitter",
-        
-          title: "a post with twitter",
-        
-        description: "an example of a blog post with twitter",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2020/twitter/";
-          
-        },
-      },{id: "post-a-post-with-disqus-comments",
-        
-          title: "a post with disqus comments",
-        
-        description: "an example of a blog post with disqus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/disqus-comments/";
-          
-        },
-      },{id: "post-a-post-with-math",
-        
-          title: "a post with math",
-        
-        description: "an example of a blog post with some math",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/math/";
-          
-        },
-      },{id: "post-a-post-with-code",
-        
-          title: "a post with code",
-        
-        description: "an example of a blog post with some code",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/code/";
-          
-        },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/images/";
-          
-        },
-      },{id: "post-a-post-with-formatting-and-links",
-        
-          title: "a post with formatting and links",
-        
-        description: "march &amp; april, looking forward to summer",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2015/formatting-and-links/";
+            window.location.href = "/blog/2023/books-in-2020/";
           
         },
       },{id: "books-the-godfather",
@@ -376,16 +90,29 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-honoured-to-receive-the-governor-general-of-canada-s-academic-gold-medal-for-outstanding-academic-achievements-during-my-master-s-at-éts",
+          title: 'Honoured to receive the Governor General of Canada’s Academic Gold Medal for outstanding...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-i-gave-a-talk-on-automatic-segmentation-of-brain-and-spinal-cord-lesions-across-pathologies-at-the-unique-fellows-get-together-held-at-mila",
+          title: 'I gave a talk on Automatic Segmentation of Brain and Spinal Cord Lesions...',
           description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-our-recent-works-on-contrast-agnostic-spinal-cord-segmentation-and-segmentation-of-spinal-cord-injury-lesions-were-accepted-for-oral-presentations-at-the-2024-ismrm-amp-amp-ismrt-annual-meeting-amp-amp-exhibition-in-singapore-singapore",
+          title: 'Our recent works on contrast-agnostic spinal cord segmentation and segmentation of spinal cord...',
+          description: "",
+          section: "News",},{id: "news-i-was-awarded-the-daad-short-term-research-grant-for-a-4-month-research-stay-at-the-technical-university-of-munich-germany",
+          title: 'I was awarded the DAAD Short-term Research Grant for a 4-month research stay...',
+          description: "",
+          section: "News",},{id: "news-our-paper-scisegv2-a-universal-tool-for-segmentation-of-intramedullary-lesions-in-spinal-cord-injury-was-accepted-at-miccai-applications-of-medical-artificial-intelligence-amai-workshop-2024-in-marrakech-morocco",
+          title: 'Our paper “SCIsegV2: A Universal Tool for Segmentation of Intramedullary Lesions in Spinal...',
+          description: "",
+          section: "News",},{id: "news-our-paper-towards-contrast-agnostic-soft-segmentation-of-the-spinal-cord-was-accepted-at-the-medical-image-analysis-journal-impact-factor-10-7",
+          title: 'Our paper “Towards contrast-agnostic soft segmentation of the spinal cord” was accepted at...',
+          description: "",
+          section: "News",},{id: "news-i-was-featured-in-the-unique-québec-newsletter-as-part-of-their-student-spotlight-series-please-find-the-newsletter-here",
+          title: 'I was featured in the UNIQUE-Québec Newsletter as part of their student spotlight...',
+          description: "",
+          section: "News",},{id: "news-our-paper-dynamic-robot-assisted-surgery-with-hierarchical-class-incremental-semantic-segmentation-was-accepted-at-miccai-workshop-on-applications-of-medical-artificial-intelligence-amai-2025-in-daejon-south-korea",
+          title: 'Our paper “Dynamic Robot-Assisted Surgery with Hierarchical Class-Incremental Semantic Segmentation” was accepted at...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
